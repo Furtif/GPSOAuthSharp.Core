35213c684fcfffe6bb3c79a64dcfbb2fe42f89be
AeonLucid <aeonlucid@outlook.com>
2016-11-09 02:01:16 +0100

    Ported to .netstandard1.1.

--- README.md
+++ README.md
@@ -1,31 +1,24 @@
-# GPSOAuthSharp
+# GPSOAuthSharp.NetStandard1
+
 A .NET client library for Google Play Services OAuth written in C#.
 
 This is a C# port of https://github.com/simon-weber/gpsoauth
 
-## NuGet package
-You can find this on NuGet at https://www.nuget.org/packages/GPSOAuthSharp/
+# Installation
 
-## Usage
-Construct a `DankMemes.GPSOAuthSharp.GPSOAuthClient(email, password)`.
+## Supported Platforms
+
+* .NET Standard 1.1 ([Specific platforms](https://github.com/dotnet/corefx/blob/master/Documentation/architecture/net-platform-standard.md#mapping-the-net-platform-standard-to-platforms))
+
+## Installation
+
+Installation is done via NuGet:
+
+    PM> Install-Package GPSOAuthSharp.NetStandard1
+    
+https://www.nuget.org/packages/GPSOAuthSharp.NetStandard1/
+
+# Usage
+Construct a `GPSOAuthSharp.GPSOAuthClient(email, password)`.
 
 Use `PerformMasterLogin()` or `PerformOAuth()` to retrieve a `Dictionary<string, string>` of response values. 
-
-Demo response values: 
-
-![](http://i.imgur.com/v5PqdKe.png)
-
-Wrong credentials:
-
-![](http://i.imgur.com/ubakOF3.png)
-
-You can download an executable for the Demo on the [Releases page](https://github.com/vemacs/GPSOAuthSharp/releases/). 
-
-The source for the Demo is located in the `GPSOAuthDemo` directory. The [main class](https://github.com/vemacs/GPSOAuthSharp/blob/master/GPSOAuthDemo/GPSOAuthDemo/Program.cs) is here.
-
-Python result (for comparison): 
-
-![](http://i.imgur.com/JyLnAK5.png)
-
-## Goals
-This project intends to follow the Google-specific parts of the Python implementation extremely carefully, so that any changes made to the Python implementation can be easily applied to this.
--- demo/GPSOAuthSharp.Demo/Program.cs
+++ demo/GPSOAuthSharp.Demo/Program.cs
@@ -1,14 +1,13 @@
 ﻿using System;
-using GPSOAuthSharp;
 
-namespace GPSOAuthDemo
+namespace GPSOAuthSharp.Demo
 {
-    internal static class Program
+    public class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var email = "";
-            var password = "";
+            var email = Environment.GetEnvironmentVariable("GOOGLE_USERNAME") ?? "";
+            var password = Environment.GetEnvironmentVariable("GOOGLE_PASSWORD") ?? "";
 
             var googleClient = new GPSOAuthClient(email, password);
             var masterLoginResponse = googleClient.PerformMasterLogin().Result;
@@ -17,18 +16,19 @@ namespace GPSOAuthDemo
             {
                 throw new Exception($"Google returned an error message: '{masterLoginResponse["Error"]}'");
             }
+
             if (!masterLoginResponse.ContainsKey("Token"))
             {
                 throw new Exception("Token was missing from master login response.");
             }
-            var oauthResponse = googleClient.PerformOAuth(masterLoginResponse["Token"], "audience:server:client_id:848232511240-7so421jotr2609rmqakceuu1luuq0ptb.apps.googleusercontent.com",
-                "com.nianticlabs.pokemongo", "321187995bc7cdc2b5fc91b11a96e2baa8602c62").Result;
+
+            var oauthResponse = googleClient.PerformOAuth(masterLoginResponse["Token"], "audience:server:client_id:848232511240-7so421jotr2609rmqakceuu1luuq0ptb.apps.googleusercontent.com","com.nianticlabs.pokemongo", "321187995bc7cdc2b5fc91b11a96e2baa8602c62").Result;
             if (!oauthResponse.ContainsKey("Auth"))
             {
                 throw new Exception("Auth token was missing from oauth login response.");
             }
-            Console.WriteLine("Authenticated through Google.");
 
+            Console.WriteLine("Authenticated through Google.");
             Console.ReadLine();
         }
     }
